0d29742fb5b0a4b3b61e42c09301984d9422e55c
Yeheni Narthana Wijesinghe <yeheninw00@gmail.com>
2025-03-11 19:18:18 +0530

    Perform arithmetic operations

---
+++ Day_01/arithmetic_operations.js
@@ -0,0 +1,12 @@
+let num1 = 10
+let num2 = 5
+
+let sum = num1+num2
+let sub = num1-num2
+let mul = num1*num2
+let div = num1/num2
+
+console.log("Summation: "+sum)
+console.log("Subtraction: "+sub)
+console.log("Multiplication: "+mul)
+console.log("Division: "+div)
