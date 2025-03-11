fba1dd2e36aec7769c348db6790603b801e5fdf8
Yeheni Narthana Wijesinghe <yeheninw00@gmail.com>
2025-03-11 19:36:58 +0530

    Reverse the numbers in left and right 4321-5-9876

---
+++ Day_01/question_04.js
@@ -0,0 +1,15 @@
+//reverse the numbers in left and right 4321-5-9876
+var i = 9
+var mid = parseInt(i/2) + 1;
+
+for(var j= mid - 1; j>0; j--)
+{
+	console.log(j)
+}
+
+console.log(mid);
+
+for(var j = i; j>mid; j--)
+{
+	console.log(j)
+}
